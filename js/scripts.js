19fadff59bfa6c1f08274b3904cb2c7afc2921aa
Rhonda <rgoolsgy@trivantage.com>
2019-07-19 16:36:47 -0400

    add toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 $('.toggle').click(function() {
-
+  $(this).toggleClass('fas fa-caret-right fas fa-caret-down');
 
     var $this = $(this);
 
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
         $this.next().slideUp(350);
+
     } else {
         $this.parent().parent().find('li .inner').removeClass('show');
         $this.parent().parent().find('li .inner').slideUp(350);
@@ -13,13 +14,3 @@ $('.toggle').click(function() {
         $this.next().slideToggle(350);
     }
 });
-
-var tada = document.getElementsByClassName("toggle");
-
-for(var i = 0; i < tada.length; i++){
-   tada[i].addEventListener("click", function(){
-    this.nextElementSibling.classList.toggle("visible");
-    this.firstElementChild.classList.toggle("hide-right-caret");
-    this.lastElementChild.classList.toggle("show-down-caret");
-  });
-}
