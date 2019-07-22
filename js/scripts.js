f75d053849c3628ba3629c401746e733ffef8d89
Rhonda <rgoolsgy@trivantage.com>
2019-07-22 17:22:32 -0400

    Add font awesome icons back

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
 $('.toggle').click(function() {
+
     var $this = $(this);
 
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
         $this.next().slideUp(350);
-        $(this).children().toggleClass("ui-icon-caret-1-e ui-icon-caret-1-e");
+        $(this).children().toggleClass("fa-angle-up fa-angle-down");
 
     } else {
         $this.parent().parent().find('li .inner').removeClass('show');
         $this.parent().parent().find('li .inner').slideUp(350);
-        $(".toggle i").attr("class", "ui-icon-caret-1-e");
+        $(".toggle i").attr("class", "fa fa-angle-down");
         $this.next().toggleClass('show');
         $this.next().slideToggle(350);
-        $(this).children().toggleClass("ui-icon-caret-1-e ui-icon-caret-1-e");
+        $(this).children().toggleClass("fa-angle-up fa-angle-down");
 
     }
 });
