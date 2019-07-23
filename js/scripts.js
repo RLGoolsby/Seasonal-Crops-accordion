5708809b48670d79c8c514c806f58fd56dd86bd7
Rhonda <rgoolsgy@trivantage.com>
2019-07-23 12:32:26 -0400

    return to original code

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,15 @@ $('.toggle').click(function() {
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
         $this.next().slideUp(350);
-        $(this).children().toggleClass("fa-angle-down fa-angle-up");
+        $(this).children().toggleClass("fa-angle-up fa-angle-down");
 
     } else {
         $this.parent().parent().find('li .inner').removeClass('show');
         $this.parent().parent().find('li .inner').slideUp(350);
-        $(".toggle i").attr("class", "fa fa-angle-up");
+        $(".toggle i").attr("class", "fa fa-angle-down");
         $this.next().toggleClass('show');
         $this.next().slideToggle(350);
-        $(this).children().toggleClass("fa-angle-down fa-angle-up");
+        $(this).children().toggleClass("fa-angle-up fa-angle-down");
 
     }
 });
