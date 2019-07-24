fd5b374ee2c2f1562ca9e6a9df664b105e1ce33d
Rhonda <rgoolsgy@trivantage.com>
2019-07-24 13:16:22 -0400

    add toggle for inner only

--- js/scripts.js
+++ js/scripts.js
@@ -5,15 +5,23 @@ $('.toggle').click(function() {
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
         $this.next().slideUp(350);
-        $(this).toggleClass("fas fa-plus fas fa-minus");
-
-    } else {
-        $this.parent().parent().find('li .inner').removeClass('show');
-        $this.parent().parent().find('li .inner').slideUp(350);
-        //$(".toggle i").attr("class", "fas fa-plus");
-        $this.next().toggleClass('show');
-        $this.next().slideToggle(350);
         $(this).children().toggleClass("fas fa-plus fas fa-minus");
 
-    }
+    // } else {
+    //     $this.parent().parent().find('li .inner').removeClass('show');
+    //     $this.parent().parent().find('li .inner').slideUp(350);
+    //     //$(".toggle i").attr("class", "fas fa-plus");
+    //     $this.next().toggleClass('show');
+    //     $this.next().slideToggle(350);
+    //     $(this).children().toggleClass("fas fa-plus fas fa-minus");
+    //
+    // }
+});
+
+$('.inner').click(function()
+if $('.inner').next().hasClass('show')){
+$this.next().removeClass('show');
+$this.next().slideUp(350);
+$(this).children().toggleClass("fas fa-plus fas fa-minus");
+}
 });
