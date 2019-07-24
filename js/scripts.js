31bae511aa558a2e68f6687e15ec34b65cc0e021
Rhonda <rgoolsgy@trivantage.com>
2019-07-24 13:17:34 -0400

    script

--- js/scripts.js
+++ js/scripts.js
@@ -7,15 +7,15 @@ $('.toggle').click(function() {
         $this.next().slideUp(350);
         $(this).children().toggleClass("fas fa-plus fas fa-minus");
 
-    // } else {
-    //     $this.parent().parent().find('li .inner').removeClass('show');
-    //     $this.parent().parent().find('li .inner').slideUp(350);
-    //     //$(".toggle i").attr("class", "fas fa-plus");
-    //     $this.next().toggleClass('show');
-    //     $this.next().slideToggle(350);
-    //     $(this).children().toggleClass("fas fa-plus fas fa-minus");
-    //
-    // }
+    } else {
+        $this.parent().parent().find('li .inner').removeClass('show');
+        $this.parent().parent().find('li .inner').slideUp(350);
+        //$(".toggle i").attr("class", "fas fa-plus");
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+        $(this).children().toggleClass("fas fa-plus fas fa-minus");
+
+    }
 });
 
 $('.inner').click(function()
