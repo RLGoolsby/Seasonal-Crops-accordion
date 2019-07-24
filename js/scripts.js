02eace45ca5b767abe15d41ebb92f954b660908f
Rhonda <rgoolsgy@trivantage.com>
2019-07-24 13:04:21 -0400

    remove children tag in script

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,7 @@ $('.toggle').click(function() {
     if ($this.next().hasClass('show')) {
         $this.next().removeClass('show');
         $this.next().slideUp(350);
-        $(this).children().toggleClass("fas fa-plus fas fa-minus");
+        $(this).toggleClass("fas fa-plus fas fa-minus");
 
     } else {
         $this.parent().parent().find('li .inner').removeClass('show');
