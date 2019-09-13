4451491c09150da0fd9f779ab80da26cd5df160c
Rhonda <rgoolsgy@trivantage.com>
2019-09-13 17:36:57 -0400

    redesign of the whole program

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,38 @@
-$('.toggle').click(function() {
 
-    var $this = $(this);
+//JQUERY ACCORDION TOGGLE AND REPOSITION ON PAGE WITH ANIMATE//
+$(document).ready (function(){
+$('.accordion').click(function(e){
+e.preventDefault();
+$(this).toggleClass('active');
+$(this).next('.inner').slideToggle("slow", "linear", function(){
+$('html,body').animate({scrollTop: $(this).offset().top -150}, 1000);
+});//END TOGGLE FUNCTION
+});//END ACCORDION CLICK EVENT
+});//END DOCUMENT.READY FUNCTION
+//////////////////////////////////////////////////////////////////////////
 
-    if ($this.next().hasClass('show')) {
-        $this.next().removeClass('show');
-        $this.next().slideUp(350);
-        $(this).children().toggleClass("fas fa-plus fas fa-minus");
+////////FOR GOOGLE FORM DISPLAY/////////////////////////////////////////////
+$(document).ready(function(){
+  $("#comment-form").click(function(){
+      $('.embed-container').slideToggle(1000, function(){
+      $('html,body').animate({scrollTop: $(this).offset().top -500}, 2000);
+    });//ENDS CLICK BUTTON FUNCTON//
+  });//ENDS FORM TOGGLE FUNCTION//
+});//ENDS DOC .READY FUNCTION//
+//////////////////////////////////////////////////////////////////////////
 
-    } else {
-        $this.parent().find('li .inner').removeClass('show');
-        $this.parent().find('li .inner').slideUp(350);
-        $("i").attr("class", "fas fa-plus");
-        $this.next().toggleClass('show');
-        $this.next().slideToggle(350);
-        $(this).children().toggleClass("fas fa-minus fas fa-plus");
-
-    }
+///EVENT THAT OPENS THE SPECIFIC ACCORDION PANELS TIED TO HREF LINK//////
+$(document).ready(function(){
+  $('.accordion').click(function(e){
+      $(this).attr('href');
+      $(this).next('.inner').toggleClass("slow", function(){
+      $('html,body').animate({scrollTop: $(this).offset().top -500}, 2000);
+    });//ENDS FORM TOGGLE FUNCTION//
+  });
+	// Begin script to open accordion panels via an external link with hash
+	var hash = window.location.hash;
+	var anchor = $('a[href$="'+hash+'"]');
+	if (anchor.length > 0){
+		anchor.click();
+	}
 });
