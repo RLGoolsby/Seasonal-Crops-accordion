0a6890ee007c4d558f9f29c2e7e6be3f74384627
Rhonda Goolsby <rlgoolsby@gmail.com>
2019-10-31 11:18:22 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,6 +18,7 @@ $(document).ready(function(){
       $('html,body').animate({scrollTop: $(this).offset().top -500}, 2000);
     });//ENDS CLICK BUTTON FUNCTON//
 	  $('#comment-form').find("input[type=radio").val("");
+	  $("#comment-form").find('input:radio').removeAttr('selected');
   });//ENDS FORM TOGGLE FUNCTION//
 });//ENDS DOC .READY FUNCTION//
 //////////////////////////////////////////////////////////////////////////
